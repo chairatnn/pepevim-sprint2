2965761d71c9b3b7579bbf5fddb99b500cedd393
PatcharaP <itt.patchara@gmail.com>
2025-12-19 22:45:16 +0700

    contact&about from aing

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { Button } from "@/components/ui/button";
 import Navbar from "./components/Navbar";
 import HomeView from "./views/HomeView";
 import PaymentView from "./views/PaymentView";
+import { AboutUsView } from "./views/AboutUsView";
+import { ContactView } from "./views/ContactView";
 function App() {
   return (
     <>
       <Navbar className="w-full" />
       {/* <HomeView /> */}
-      <PaymentView/>
+      {/* <PaymentView/> */}
+      {/* <AboutUsView/> */}
+      {/* <ContactView/> */}
     </>
   );
 }
